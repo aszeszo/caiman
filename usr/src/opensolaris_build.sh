dbb4fb4e78bb09c3747567c923d88f51fae3fa55
"mw145384 ext:(%22) <moriah.waterland@sun.com>
2007-12-05 09:47:03 -0800

    283 cannot build slim_source

--- usr/src/opensolaris_build.sh
+++ usr/src/opensolaris_build.sh
@@ -40,11 +40,12 @@ fi
 
 # setup the proto area, creating the necessary directory tree
 rm -rf $ROOT
-dmake -k -m serial proto >/dev/null 2>&1
+dmake -k -m serial proto
 
 # create links to snadm libraries in proto area
 ln -s /usr/snadm/lib/libadmutil.so $ROOT/usr/snadm/lib 
 ln -s /usr/snadm/lib/libadmldb.so $ROOT/usr/snadm/lib 
 
 # Build and install the individual components
-dmake -e install
+dmake -e -k -m parallel install
+
