ec2b440d337997b482ce8af79a8958bd16987df8
Matt <matt.keenan@sun.com>
2009-02-06 18:05:16 +0000

    8786 String "No default language support" not extracted to gui-install.pot

--- usr/src/cmd/gui-install/src/orchestrator-wrappers.c
+++ usr/src/cmd/gui-install/src/orchestrator-wrappers.c
@@ -19,7 +19,7 @@
  * CDDL HEADER END
  */
 /*
- * Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -33,7 +33,7 @@
 
 #include "orchestrator-wrappers.h"
 
-#define	NODEFAULTLANGLABEL "No default language support"
+#define	NODEFAULTLANGLABEL N_("No default language support")
 
 static float
 round_up_to_tenths(float value)
