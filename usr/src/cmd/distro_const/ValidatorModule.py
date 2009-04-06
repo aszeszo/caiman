5f57efe3f34fef1f89e46e4ca2fdd078d8ae4b66
Jan Damborsky <jan.damborsky@sun.com>
2009-04-06 09:51:01 +0200

    6492 DC manifest does not allow for fully qualified FMRI package names 7784 Install fails when specifying branch (build) in manifest

--- usr/src/cmd/distro_const/ValidatorModule.py
+++ usr/src/cmd/distro_const/ValidatorModule.py
@@ -18,7 +18,7 @@
 #
 # CDDL HEADER END
 #
-# Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+# Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
 # Use is subject to license terms.
 
 # =============================================================================
@@ -62,7 +62,7 @@ class ValidatorModule:
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	# Returns True if disallowed_chars are found in the node's value.
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		disallowed_chars = "*%?@[]{}|><()#$\"\'\\"
+		disallowed_chars = "*%?[]{}|><()#$\"\'\\"
 		value = node.get_value()
 		for dchar in disallowed_chars:
 			if (value.find(dchar) != -1):
