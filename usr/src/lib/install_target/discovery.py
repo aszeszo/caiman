ca1b8bb7eb75a4069069a985e14af96565bd0fba
Drew Fisher <drew.fisher@oracle.com>
2011-08-19 18:35:38 -0600

    7077874 test_td.py cannot be run in installed environment 7081079 discovery should stop finding 'dump' as a drive

--- usr/src/lib/install_target/discovery.py
+++ usr/src/lib/install_target/discovery.py
@@ -30,9 +30,11 @@ information.
 """
 
 import logging
+import optparse
 import os
 import platform
 import re
+import sys
 
 import solaris_install.target.vdevs as vdevs
 
@@ -621,7 +623,7 @@ class TargetDiscovery(Checkpoint):
             new_disk = self.discover_disk(drive)
 
             # skip invalid drives and CDROM drives
-            if new_disk is None or new_disk.iscdrom:
+            if new_disk is None or new_disk.iscdrom or new_disk.ctd == "dump":
                 continue
 
             # skip any disk we've already discovered
@@ -926,3 +928,227 @@ class TargetDiscovery(Checkpoint):
 
         # Add the root node to the DOC
         self.doc.persistent.insert_children(self.root)
+
+
+if __name__ == "__main__":
+    # if discovery.py is run from the command line, rather than imported as a
+    # checkpoint, run discovery and print the output.
+    def parse_args():
+        """ parse_args() - function to parse the command line arguments
+        """
+        parser = optparse.OptionParser()
+        parser.add_option("-d", "--disk", dest="disk", action="store_true",
+                          help="print disk information")
+        parser.add_option("-p", "--partition", dest="partition",
+                          action="store_true",
+                          help="print partition information")
+        parser.add_option("-s", "--slice", dest="slc", action="store_true",
+                          help="print slice information")
+        parser.add_option("-z", "--zpool", dest="zpool", action="store_true",
+                          help="print zpool information")
+        parser.add_option("--libdiskmgt", dest="libdiskmgt",
+                          action="store_true",
+                          help="print all output from libdiskmgt")
+        parser.add_option("--xml", dest="xml", action="store_true",
+                          help="print DOC xml")
+        options, args = parser.parse_args()
+
+        # only run target discovery if the user requests something out of the
+        # DOC.  If only libdiskmgt output is requested, do not run target
+        # discovery.
+        if options.disk or options.partition or options.slc or \
+           options.zpool or options.xml:
+            options.require_td = True
+        else:
+            options.require_td = False
+
+        return options, args
+
+    def print_disk(doc):
+        """ prints the output from the -d argument
+        """
+        print "Disk discovery"
+        # walk the DOC and print only what the user requested
+        target = doc.persistent.get_children(class_type=Target)[0]
+        disk_list = target.get_children(class_type=Disk)
+        header_format = "%15s | %15s | %15s |"
+        entry_format = "%15s | %15s | %15.3f |"
+        print "Total number of disks:  %d" % len(disk_list)
+        print '-' * 53
+        print header_format % ("name", "boot disk?", "size [GB]")
+        print '-' * 53
+        for disk in disk_list:
+            if disk.disk_keyword is not None and \
+               disk.disk_keyword.key == "boot_disk":
+                boot = "yes"
+            else:
+                boot = "no"
+            print entry_format % (disk.ctd, boot, \
+                                  disk.disk_prop.dev_size.get(Size.gb_units))
+        print '-' * 53
+
+    def print_partition(doc):
+        """ prints the output from the -p argument
+        """
+        print "Partition discovery"
+        # walk the DOC and print only what the user requested
+        target = doc.persistent.get_children(class_type=Target)[0]
+        disk_list = target.get_children(class_type=Disk)
+        header_format = "{0:>15} | {1:>5} | {2:>7} | {3:>5} | {4:>11} |"
+        disk_format = "{0:>15} |" + 39 * " " + "|"
+        line_format = " " * 16 + "| {0:>5} | {1:>7} | {2:>5} | {3:>11} |"
+
+        entry_line = '-' * 57
+
+        print entry_line
+        print header_format.format(*["disk_name", "index", "active?", "ID",
+                                   "Linux Swap?"])
+        print entry_line
+        for disk in disk_list:
+            print disk_format.format(disk.ctd)
+            partition_list = disk.get_children(class_type=Partition)
+            for partition in partition_list:
+                if partition.bootid == Partition.ACTIVE:
+                    active = "yes"
+                else:
+                    active = "no"
+
+                if partition.is_linux_swap:
+                    linux_swap = "yes"
+                else:
+                    linux_swap = "no"
+                print line_format.format(*[partition.name.split("/")[-1],
+                                         active, partition.part_type,
+                                         linux_swap])
+            print entry_line
+
+    def print_slice(doc):
+        """ prints the output from the -s argument
+        """
+        print "Slice discovery"
+        # walk the DOC and print only what the user requested
+        target = doc.persistent.get_children(class_type=Target)[0]
+        disk_list = target.get_children(class_type=Disk)
+
+        header_format = "{0:>15} | {1:>5} | {2:>40} |"
+        disk_format = "{0:>15} |" + 7 * " " + "|" + 42 * " " + "|"
+        entry_format = " " * 16 + "| {0:>5} | {1:>40} |"
+
+        disk_line = '-' * 67
+        entry_line = disk_line + "|"
+
+        print disk_line
+        print header_format.format(*["disk name", "index", "in use?"])
+        print entry_line
+        for disk in disk_list:
+            print disk_format.format(disk.ctd)
+            slice_list = disk.get_descendants(class_type=Slice)
+            for slc in slice_list:
+                if slc.in_use is None:
+                    in_use = "no"
+                else:
+                    if "used_by" in slc.in_use:
+                        in_use = "used by:  %s" % slc.in_use["used_by"][0]
+                        if "used_name" in slc.in_use:
+                            in_use += " (%s)" % slc.in_use["used_name"][0]
+                print entry_format.format(*[slc.name, in_use])
+            print entry_line
+
+    def print_zpool(doc):
+        """ prints the output from the -z argument
+        """
+        print "Zpool discovery"
+        target = doc.persistent.get_children(class_type=Target)[0]
+        logical = target.get_children(class_type=Logical)[0]
+        zpool_list = logical.get_children(class_type=Zpool)
+
+        print_format = "{0:>10} | {1:>20} | {2:>20} | {3:>7} | {4:>8} |"
+
+        zpool_line = '-' * 79
+
+        print zpool_line
+        print print_format.format(*["name", "bootfs", "guid", "size",
+                                    "capacity"])
+        print zpool_line
+        for zpool in zpool_list:
+            # extract zpool information
+            cmd = ["/usr/sbin/zpool", "get", "bootfs,guid,size,capacity",
+                   zpool.name]
+            p = Popen.check_call(cmd, stdout=Popen.STORE, stderr=Popen.STORE)
+            # strip out just the third column.  Exclude the header row and
+            # trailing newline
+            (bootfs, guid, size, capacity) = \
+                [line.split()[2] for line in p.stdout.split("\n")[1:-1]]
+            print print_format.format(*[zpool.name, bootfs, guid, size,
+                                        capacity])
+        print zpool_line
+
+    def print_libdiskmgt():
+        """ prints drive information from the --libdiskmgt argument
+        """
+        
+        def indent_str(level, string):
+            """ function to print an indented string
+            """
+            return "\n".join(
+                ["    " * level + line for line in string.splitlines()]
+            )
+
+        # start at the bus level
+        indent = 0
+        for bus in diskmgt.descriptors_by_type(const.BUS):
+            print indent_str(indent, str(bus))
+            indent += 1
+            for controller in bus.controllers:
+                print indent_str(indent, str(controller))
+                indent += 1
+                for path in controller.paths:
+                    print indent_str(indent, str(path))
+                for drive in controller.drives:
+                    print indent_str(indent, str(drive))
+                    indent += 1
+                    for path in drive.paths:
+                        print indent_str(indent, str(path))
+                    for alias in drive.aliases:
+                        print indent_str(indent, str(alias))
+
+                    media = drive.media
+                    if media is not None:
+                        print indent_str(indent, str(media))
+                        dma = media.attributes
+                        indent += 1
+                        if dma.efi or dma.fdisk:
+                            for part in media.partitions:
+                                print indent_str(indent, str(part))
+                        for slc in media.slices:
+                            print indent_str(indent, str(slc))
+                        indent -= 1
+                    indent -= 1
+                indent -= 1
+            indent -= 1
+
+    # parse command line arguments
+    options, args = parse_args()
+
+    if options.libdiskmgt:
+        print_libdiskmgt()
+        if not options.require_td:
+            sys.exit()
+
+    # set up Target Discovery and execute it, finding the entire system
+    InstallEngine()
+    TD = TargetDiscovery("Test TD")
+
+    # set dry_run to True so we don't label any drives on SPARC
+    TD.execute(dry_run=True)
+
+    if options.disk:
+        print_disk(TD.doc)
+    if options.partition:
+        print_partition(TD.doc)
+    if options.slc:
+        print_slice(TD.doc)
+    if options.zpool:
+        print_zpool(TD.doc)
+    if options.xml:
+        print TD.doc.get_xml_tree_str()
